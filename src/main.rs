80eddf66e15a6d14fd7c75e58cfb98c9528aad91
dhilipsiva <dhilipsiva@pm.me>
2023-04-12 11:29:13 +0530

    match pairs

--- src/main.rs
+++ src/main.rs
@@ -8,69 +8,81 @@ mod parser {
 
 use parser::Rule;
 use pest::{
-    iterators::Pairs,
+    iterators::Pair,
     // pratt_parser::{Assoc::*, Op, PrattParser},
     Parser,
 };
 use std::fs::read_to_string;
 
-/*
-enum Atom {
-    Int,
-    Float,
-    String,
-}
-
-enum Value {
-    Atom(Atom),
-    List(Box<Value>),
-    Dict(Box<Value>),
-}
-
-fn oduraja(pairs: Pairs<Rule>, pratt: &PrattParser<Rule>) {
-    pratt
-        .map_primary(|primary| match primary.as_rule() {
-            Rule::int => Value::Atom(primary.as_str().parse::<i32>().unwrap()),
-            Rule::expression => Value::Atom(oduraja(primary.into_inner(), pratt)),
-            _ => unreachable!(),
-        })
-        .map_prefix(|op, rhs| match op.as_rule() {
-            Rule::neg => -rhs,
-            _ => unreachable!(),
-        })
-        .map_infix(|lhs, op, rhs| match op.as_rule() {
-            Rule::add => lhs + rhs,
-            Rule::sub => lhs - rhs,
-            Rule::mul => lhs * rhs,
-            Rule::div => lhs / rhs,
-            _ => unreachable!(),
-        })
-        .parse(pairs)
-}
-*/
-
-fn oduraja(tree: &mut Pairs<Rule>) {
-    dbg!(&tree);
-    let _pairs = tree
-        .next()
-        .unwrap()
-        .into_inner() // inner of program
-        .next()
-        .unwrap()
-        .into_inner(); // inner of expr
-                       /*
-                       let _pratt = PrattParser::new()
-                           .op(Op::infix(Rule::add, Left) | Op::infix(Rule::sub, Left))
-                           .op(Op::infix(Rule::mul, Left) | Op::infix(Rule::div, Left))
-                           .op(Op::prefix(Rule::neg));
-                       */
+fn oduraja(pair: Pair<Rule>) {
+    match pair.as_rule() {
+        Rule::EOI => todo!(),
+        Rule::WHITESPACE => todo!(),
+        Rule::oduraja => todo!(),
+        Rule::reserved => todo!(),
+        Rule::part => todo!(),
+        Rule::stmt_invoke => todo!(),
+        Rule::stmt_define => todo!(),
+        Rule::param_invoke => todo!(),
+        Rule::param_define => todo!(),
+        Rule::COMMENT => todo!(),
+        Rule::comment_block => todo!(),
+        Rule::comment_line => todo!(),
+        Rule::expression => todo!(),
+        Rule::infix => todo!(),
+        Rule::expression_inner => todo!(),
+        Rule::braced_expression => todo!(),
+        Rule::unary => todo!(),
+        Rule::dot_path => todo!(),
+        Rule::literal => todo!(),
+        Rule::array => todo!(),
+        Rule::atom => todo!(),
+        Rule::ident => todo!(),
+        Rule::map => todo!(),
+        Rule::map_pair => todo!(),
+        Rule::keyword => todo!(),
+        Rule::integer => todo!(),
+        Rule::float => todo!(),
+        Rule::string => todo!(),
+        Rule::string_content => todo!(),
+        Rule::string_delimiter => todo!(),
+        Rule::string_escape => todo!(),
+        Rule::exponent => todo!(),
+        Rule::multiply => todo!(),
+        Rule::divide => todo!(),
+        Rule::modulus => todo!(),
+        Rule::plus => todo!(),
+        Rule::minus => todo!(),
+        Rule::less_than => todo!(),
+        Rule::less_than_or_equal => todo!(),
+        Rule::greater_than => todo!(),
+        Rule::greater_than_or_equal => todo!(),
+        Rule::not_equal => todo!(),
+        Rule::equal => todo!(),
+        Rule::logical_and => todo!(),
+        Rule::logical_or => todo!(),
+        Rule::logical_not => todo!(),
+        Rule::binary_operator => todo!(),
+        Rule::unary_operator => todo!(),
+        Rule::boolean => todo!(),
+        Rule::boolean_true => todo!(),
+        Rule::boolean_false => todo!(),
+    };
+    /*
+    let _pratt = PrattParser::new()
+        .op(Op::infix(Rule::add, Left) | Op::infix(Rule::sub, Left))
+        .op(Op::infix(Rule::mul, Left) | Op::infix(Rule::div, Left))
+        .op(Op::prefix(Rule::neg));
+    */
 }
 fn main() {
     let source = read_to_string("examples/01-basics.oduraja").unwrap();
 
     match parser::Parser::parse(Rule::oduraja, &source) {
-        Ok(mut tree) => {
-            oduraja(&mut tree);
+        Ok(tree) => {
+            for pair in tree {
+                oduraja(pair);
+            }
         }
         Err(err) => {
             println!("Failed parsing input: {:}", err);
